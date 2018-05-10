87c6ca231aede096f138e899ad2c65fc97235973
Hanaa-Adel <32802950+Hanaa-Adel@users.noreply.github.com>
2018-05-10 21:07:47 +0200

    Update Netgroupscpp.cpp

--- Netgroupscpp.cpp
+++ Netgroupscpp.cpp
@@ -3,4 +3,15 @@
 
 using namespace std ;
 
-Netgroups::Netgroups(){} //default conestructor
+
+
+Netgroups::Netgroups()
+{
+	group_id=0;
+	group_name=" ";
+
+} //default conestructor
+Netgroups::Netgroups(string group_name)
+{
+	this ->group_name=group_name;
+}
